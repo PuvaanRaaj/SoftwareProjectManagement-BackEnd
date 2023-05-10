4e88682d982f5e70bdc80911bab5807df9575372
Puvaan Raaj <112681813+PuvaanRaaj@users.noreply.github.com>
2023-05-11 01:08:23 +0800

    Update getTokenFromHeader.js

--- utils/getTokenFromHeader.js
+++ utils/getTokenFromHeader.js
@@ -1,4 +1,4 @@
-getexport const getTokenFromHeader = (req) => {
+export const getTokenFromHeader = (req) => {
   //get token from header
   const token = req?.headers?.authorization?.split(" ")[1];
   if (token === undefined) {
